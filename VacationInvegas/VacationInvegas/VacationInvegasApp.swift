18fbf6a607f9f5f773f00c5a842b74525db7b49a
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-25 11:10:59 +0800

    Initial commit

---
+++ VacationInvegas/VacationInvegas/VacationInvegasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInvegasApp.swift
+//  VacationInvegas
+//
+//  Created by 张朕 on 2024/12/25.
+//
+
+import SwiftUI
+
+@main
+struct VacationInvegasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
