92fff6d1689bd635c5a8e3472d3011620ff09ff8
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-25 13:04:36 +0800

    changes

--- VacationInvegas/VacationInvegas/Symbols.swift
+++ VacationInvegas/VacationInvegas/Symbols.swift
@@ -7,19 +7,58 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Symbols: View {
+    @State private var shouldBounce = false
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .font(.largeTitle)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .symbolEffect(.pulse)
+            
+            Image(systemName: "airplane")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.teal)
+                .symbolEffect(.wiggle)
+            
+            Image(systemName: "wifi")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.purple)
+                .symbolEffect(.variableColor.reversing)
+            
+            Image(systemName: "person.crop.circle.badge.plus")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+                .symbolRenderingMode(.multicolor)
+                .symbolEffect(.bounce, value: shouldBounce)
+                .onTapGesture {
+                    shouldBounce.toggle()
+                }
+            
+            Image(systemName: "cloud.sun.rain.fill")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            
+            Image(systemName: "globe")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            
+            Image(systemName: "globe")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    Symbols()
 }
