8d2c64bc1876b9efd9eb5c7ee541665a3711b047
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-26 10:45:35 +0800

    changes

---
+++ VacationInvegas/VacationInvegas/ScrollImage.swift
@@ -0,0 +1,28 @@
+//
+//  ScrollImages.swift
+//  VacationInvegas
+//
+//  Created by 张朕 on 2024/12/26.
+//
+
+import SwiftUI
+
+struct ScrollImage: View {
+    let image: ImageResource
+    
+    var body: some View {
+        Image(image)
+            .resizable()
+            .scaledToFit()
+            .clipShape(.rect(cornerRadius: 20))
+            .scrollTransition { content, phase in
+                content
+                    .scaleEffect(phase.isIdentity ? 1 : 0.5)
+                    .opacity(phase.isIdentity ? 1 : 0.5)
+            }
+    }
+}
+
+#Preview {
+    ScrollImage(image: .su719)
+}
