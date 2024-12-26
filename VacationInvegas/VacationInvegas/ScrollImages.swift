05f1eb5a8968248edec9424e39969c3862c38690
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-26 10:38:12 +0800

    changes

---
+++ VacationInvegas/VacationInvegas/ScrollImages.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollImages.swift
+//  VacationInvegas
+//
+//  Created by 张朕 on 2024/12/26.
+//
+
+import SwiftUI
+
+struct ScrollImages: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollImages()
+}
