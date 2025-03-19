1862525e9bbcb011e468013d144456a19e51c5f2
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-03-19 15:25:01 +0800

    Update MapView.swift
    调整地图

--- VacationInvegas/VacationInvegas/MapView.swift
+++ VacationInvegas/VacationInvegas/MapView.swift
@@ -10,11 +10,16 @@ import MapKit
 
 
 struct MapView: View {
+    var place: Place
+    
+    @State var position: MapCameraPosition
+    
     var body: some View {
         Map()
     }
 }
 
 #Preview {
-    MapView()
+    MapView(place: Place.previewPlace[0], position: .camera(MapCamera(
+        centerCoordinate: Place.previewPlace[0].loaction, distance: 1000, heading: 250, pitch: 80)))
 }
