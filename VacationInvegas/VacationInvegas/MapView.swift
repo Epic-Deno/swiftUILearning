820a027e9d97c20d7ee94dad602dd427fc69c146
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-03-19 15:20:40 +0800

    Create MapView.swift
    地图组件添加

---
+++ VacationInvegas/VacationInvegas/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  VacationInvegas
+//
+//  Created by 张朕 on 2024/12/25.
+//
+
+import SwiftUI
+import MapKit
+
+
+struct MapView: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    MapView()
+}
