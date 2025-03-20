320a85850881c436d25c5d91940e609fbb22b1cb
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-03-20 10:42:50 +0800

    Update VegasChart.swift
    双环

--- VacationInvegas/VacationInvegas/VegasChart.swift
+++ VacationInvegas/VacationInvegas/VegasChart.swift
@@ -23,7 +23,7 @@ struct SampleRating {
 struct VegasChart: View {
     var body: some View {
         Chart(SampleRating.ratings, id: \.place) { rating in
-            SectorMark(angle: .value("Ratings", rating.rating), angularInset: 1)
+            SectorMark(angle: .value("Ratings", rating.rating), innerRadius: .ratio(0.5), angularInset: 1)
                 .cornerRadius(7)
                 .foregroundStyle(by: .value("place",rating.place))
         }
