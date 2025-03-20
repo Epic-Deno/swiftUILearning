1d19f857a955d69fdb148434b8620f750f1b436c
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-03-20 10:40:12 +0800

    Create VegasChart.swift
    图表

---
+++ VacationInvegas/VacationInvegas/VegasChart.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  VacationInvegas
+//
+//  Created by 张朕 on 2024/12/25.
+//
+
+import SwiftUI
+import Charts
+
+struct SampleRating {
+    let place: String
+    let rating: Int
+    
+    static let ratings: [SampleRating] = [
+        SampleRating(place: "Nanjing", rating: 88),
+        SampleRating(place: "ShangHai", rating: 76),
+        SampleRating(place: "Beijing", rating: 33),
+        SampleRating(place: "TaiWang", rating: 99),
+    ]
+}
+
+struct VegasChart: View {
+    var body: some View {
+        Chart(SampleRating.ratings, id: \.place) { rating in
+            SectorMark(angle: .value("Ratings", rating.rating), angularInset: 1)
+                .cornerRadius(7)
+                .foregroundStyle(by: .value("place",rating.place))
+        }
+    }
+}
+
+#Preview {
+    VegasChart()
+}
